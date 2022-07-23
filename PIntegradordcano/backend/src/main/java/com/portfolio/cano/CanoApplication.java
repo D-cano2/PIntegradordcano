97d002d3db23e5331af17df0cd96a99543915ab2
D-cano2 <canod513@gmail.com>
2022-07-23 19:56:43 -0300

    nuevo back

---
+++ PIntegradordcano/backend/src/main/java/com/portfolio/cano/CanoApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.cano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanoApplication.class, args);
+	}
+
+}
