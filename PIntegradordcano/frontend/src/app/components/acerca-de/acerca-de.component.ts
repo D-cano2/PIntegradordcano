f8bb8897820bf50733651df44ef58636e44e9cc5
D-cano2 <canod513@gmail.com>
2022-08-18 20:47:39 -0300

    seguimosconexion

--- PIntegradordcano/frontend/src/app/components/acerca-de/acerca-de.component.ts
+++ PIntegradordcano/frontend/src/app/components/acerca-de/acerca-de.component.ts
@@ -13,7 +13,7 @@ persona: persona = new persona("","","");
   constructor(public personaService: PersonaService) { }
 
   ngOnInit(): void {
-    this.personaService.getPersona().subscribe(data =>{this.persona = data})
+    this.personaService.getPersona().subscribe(data => {this.persona = data})
   }
 
 }
