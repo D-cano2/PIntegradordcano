5b3143a37af9fc0cfe2c6856d05bf7942630421b
D-cano2 <canod513@gmail.com>
2022-09-07 20:56:50 -0300

    deploy18

--- PIntegradordcano/frontend/src/app/model/s-experiencia.service.ts
+++ PIntegradordcano/frontend/src/app/model/s-experiencia.service.ts
@@ -7,7 +7,7 @@ import { Experiencia } from './experiencia';
   providedIn: 'root'
 })
 export class SExperienciaService {
-expURL = 'http//localhost:8080/explab'
+expURL = 'https://bendportfolio.herokuapp.com/explab';
 
   constructor(private httpClient: HttpClient) { }
 
