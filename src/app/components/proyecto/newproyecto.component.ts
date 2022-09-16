8827db0328490dae8c1ee80e91419c7eb1f2d383
diego cano <canod513@gmail.com>
2022-09-16 18:33:00 -0300

    more

--- src/app/components/proyecto/newproyecto.component.ts
+++ src/app/components/proyecto/newproyecto.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./newproyecto.component.css']
 })
 export class NewproyectoComponent implements OnInit {
+onCreate() {
+throw new Error('Method not implemented.');
+}
+descripcion: any;
+nombre: any;
 
   constructor() { }
 
