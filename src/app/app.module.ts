ebc7f50fc6784a8c488e4afd3118f7a8c8666c0b
diego cano <canod513@gmail.com>
2022-09-18 12:48:25 -0300

    eliminecomponentesporerror

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,17 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
-import { ExperienciaComponent } from './components/experiencia/experiencia.component';
-import { EducacionComponent } from './components/educacion/educacion.component';
+
 import { NgCircleProgressModule } from 'ng-circle-progress';
-import { HyskillsComponent } from './components/hyskills/hyskills.component';
-import { ProyectoComponent } from './components/proyecto/proyecto.component';
+
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
+
 import { interceptorProvider } from './service/interceptor-service';
 import { FormsModule } from '@angular/forms';
-import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
-import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
-import { NeweducacionComponent } from './components/educacion/neweducacion.component';
-import { EditeducacionComponent } from './components/educacion/editeducacion.component';
-import { EditSkillComponent } from './components/hyskills/edit-skill.component';
-import { NewSkillComponent } from './components/hyskills/new-skill.component';
-import { EditproyectoComponent } from './components/proyecto/editproyecto.component';
-import { NewproyectoComponent } from './components/proyecto/newproyecto.component';
+
+
 
 
 @NgModule({
@@ -36,21 +28,10 @@ import { NewproyectoComponent } from './components/proyecto/newproyecto.componen
   
     BannerComponent,
     AcercaDeComponent,
-    ExperienciaComponent,
-    EducacionComponent,
-    HyskillsComponent,
-    ProyectoComponent,
+  
     FooterComponent,
     HomeComponent,
-    LoginComponent,
-    NewExperienciaComponent,
-    EditExperienciaComponent,
-    NeweducacionComponent,
-    EditeducacionComponent,
-    EditSkillComponent,
-    NewSkillComponent,
-    EditproyectoComponent,
-    NewproyectoComponent
+  
   ],
   imports: [
     BrowserModule,
